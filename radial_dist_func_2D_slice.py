1248903e086aa9150614bbbd3f8805c9788f2451
redo <>
2015-03-25 05:02:43 +0000

    *** empty log message ***

--- radial_dist_func_2D_slice.py
+++ radial_dist_func_2D_slice.py
@@ -11,6 +11,16 @@ import sys
 
 import numpy
 
+#####################################################################
+
+def drange(start, stop, step):
+  r = start
+  while r < stop:
+    yield r
+    r += step
+
+#####################################################################
+
 # create a rendering window and renderer
 ren = vtk.vtkRenderer()
 renWin = vtk.vtkRenderWindow()
@@ -27,11 +37,14 @@ spheres = []
 zmax = xmax = ymax = -10000.0
 zmin = xmin = ymin =  10000.0
 
+R_average = 0.0
 for sp in file:
   x, y, z, r = sp.split(" ")
   center = point(float(x), float(y), float(z))
   s = sphere(center, float(r))
   spheres.append(s)
+
+  R_average = R_average + r
 
   if (zmax < (float(z) + float(r))):
     zmax = (float(z) + float(r))
@@ -49,6 +62,10 @@ for sp in file:
 
 volume = (xmax-xmin) * (ymax-ymin) * (zmax-zmin)
 
+R_average = R_average / float(len(spheres))
+
+print "zmin -> zmax ", zmin , zmax
+
 #print "xmax: ", xmax, "xmin: ",xmin, "ymax: ", ymax, \
 #    "ymin: ", ymin, "zmx: ", zmax, "zmin: ", zmin
 
@@ -56,119 +73,117 @@ file.close()
 
 # il piano deve essere tra zmin e zmax 
 # equazione del piano
-zplane = 50.0
 
-spheres_in_plane = []
-
-R_average = 0.0
-for s in spheres:
-
-  r = s.get_radius()
-
-  c = s.get_center()
-
-  cx = c.get_x()
-  cy = c.get_y()
-  cz = c.get_z()
-
-  if (zplane >= (cz - r)) and (zplane <= (cz + r)):
-    spheres_in_plane.append(s)
-    R_average = R_average + r
-
-R_average = R_average / float(len(spheres_in_plane))
-
-circles = []
-
-for s in spheres_in_plane:
-  r = s.get_radius()
-  c = s.get_center()
-
-  cx = c.get_x()
-  cy = c.get_y()
-  cz = c.get_z()
-
-  circler = math.sqrt(math.pow(r, 2) - math.pow((zplane-cz),2))
-
-  cir = circle(cx, cy, circler)
+for zplane in drange (zmin+R_average, zmax-R_average, 2.0*R_average)
   
-  circles.append(cir)
-
-x = numpy.linspace( 0.0, 0.0, len(circles)) 
-y = numpy.linspace( 0.0, 0.0, len(circles))
-
-xmin = ymin =  100000000.0
-xmax = ymax = -100000000.0
-
-R_average_circle = 0.0
-i = 0
-for s in circles:
-  xv = 0.0
-  yv = 0.0 
-
-  xv, yv = s.get_center()
-
-  x[i] = xv
-  y[i] = yv
-
-  xv = x[i] - s.get_radius()
-  if (xv < xmin):
-    xmin = xv
-
-  yv = y[i] - s.get_radius()
-  if (yv < ymin):
-    ymin = yv
-
-  xv = x[i] + s.get_radius()
-  if (xv > xmax):
-    xmax = xv
-
-  yv = y[i] + s.get_radius()
-  if (yv > ymax):
-    ymax = yv
-
-  R_average_circle = R_average_circle + s.get_radius()
-
-  i += 1
-
-R_average_circle = R_average_circle / float(i)
-
-print "TopX: ", xmax, "BotX: ", xmin
-print "TopY: ", ymax, "BotY: ", ymin
-print ""
-print "R_average_circle: " , R_average_circle
-print "I will use R_average: " , R_average
-
-tuple_box = (xmax-xmin, ymax-ymin)
-R_max = min(min(tuple_box)/2.0, 10.0*R_average)
-dr = 0.4
-
-g_average, radii, x, y, indxs = pairCorrelationFunction_2D (x, y, \
-      max(tuple_box)+(R_average),R_max,dr)
-
+  spheres_in_plane = []
+  
+  for s in spheres:
+  
+    r = s.get_radius()
+  
+    c = s.get_center()
+  
+    cx = c.get_x()
+    cy = c.get_y()
+    cz = c.get_z()
+  
+    if (zplane >= (cz - r)) and (zplane <= (cz + r)):
+      spheres_in_plane.append(s)
+  
+  circles = []
+  
+  for s in spheres_in_plane:
+    r = s.get_radius()
+    c = s.get_center()
+  
+    cx = c.get_x()
+    cy = c.get_y()
+    cz = c.get_z()
+  
+    circler = math.sqrt(math.pow(r, 2) - math.pow((zplane-cz),2))
+  
+    cir = circle(cx, cy, circler)
+    
+    circles.append(cir)
+  
+  
+  x = numpy.linspace( 0.0, 0.0, len(circles)) 
+  y = numpy.linspace( 0.0, 0.0, len(circles))
+  
+  xmin = ymin =  100000000.0
+  xmax = ymax = -100000000.0
+  
+  R_average_circle = 0.0
+  i = 0
+  for s in circles:
+    xv = 0.0
+    yv = 0.0 
+  
+    xv, yv = s.get_center()
+  
+    x[i] = xv
+    y[i] = yv
+  
+    xv = x[i] - s.get_radius()
+    if (xv < xmin):
+      xmin = xv
+  
+    yv = y[i] - s.get_radius()
+    if (yv < ymin):
+      ymin = yv
+  
+    xv = x[i] + s.get_radius()
+    if (xv > xmax):
+      xmax = xv
+  
+    yv = y[i] + s.get_radius()
+    if (yv > ymax):
+      ymax = yv
+  
+    R_average_circle = R_average_circle + s.get_radius()
+  
+    i += 1
+  
+  R_average_circle = R_average_circle / float(i)
+  
+  print "TopX: ", xmax, "BotX: ", xmin
+  print "TopY: ", ymax, "BotY: ", ymin
+  print ""
+  print "R_average_circle: " , R_average_circle
+  print "I will use R_average: " , R_average
+  
+  tuple_box = (xmax-xmin, ymax-ymin)
+  R_max = min(min(tuple_box)/2.0, 10.0*R_average)
+  dr = 0.4
+  
+  g_average, radii, x, y, indxs = pairCorrelationFunction_2D (x, y, \
+        max(tuple_box)+(R_average),R_max,dr)
+  
+  # per normalizzare a uno
+  #total = 1.0
+  
+  total = -1000.0
+  for i in range(0,radii.size):
+    if g_average[i] > total:
+      total = g_average[i]
+  
+  for i in range(0,radii.size):
+    data = str(radii[i]/(R_average)) + \
+        " " + str(g_average[i]/total) + "\n"
+    outf.write(data)
+  
+  sum = 0.0
+  for i in range(1,radii.size):
+  
+    if (radii[i]/R_average) < 2.9 :
+      gv = (g_average[i]/total + g_average[i-1]/total)/2.0
+      dr = radii[i]-radii[i-1]
+  
+      sum += gv*dr
+  
+  print sum
+  
 outf = open ("radial_distribution.txt", "w")
-
-# per normalizzare a uno
-#total = 1.0
-
-total = -1000.0
-for i in range(0,radii.size):
-  if g_average[i] > total:
-    total = g_average[i]
-
-for i in range(0,radii.size):
-  data = str(radii[i]/(R_average)) + \
-      " " + str(g_average[i]/total) + "\n"
-  outf.write(data)
-
-sum = 0.0
-for i in range(1,radii.size):
-
-  if (radii[i]/R_average) < 2.9 :
-    gv = (g_average[i]/total + g_average[i-1]/total)/2.0
-    dr = radii[i]-radii[i-1]
-
-    sum += gv*dr
-
-print sum
 
 outf.close()
