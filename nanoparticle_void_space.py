9c50caa9a576ac07066d02a1c0ee7ea6fa70de6f
redo <>
2015-05-25 12:54:14 +0000

    *** empty log message ***

--- nanoparticle_void_space.py
+++ nanoparticle_void_space.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 import sys
 import vtk
 
+import numpy 
+
 import random
 import math
 
@@ -77,13 +79,73 @@ print ('Box limits: '+str(minbox_x)+' '+str(minbox_y)+ \
     ' '+str(minbox_z)+' '+str(maxbox_x)+' '+ \
     str(maxbox_y)+' '+str(maxbox_z)+'\n')
 
+r = 0.0
+for nanop in nanoparticles:
+  r += nanop.get_max_sphere()
+
+mead_d = 2.0 * (r/float(len(nanoparticles)))
+
+print ('Mean D: '+str(mead_d)+'\n')
+
+px = minbox_x+2.0*mead_d
+py = minbox_y+2.0*mead_d
+pz = minbox_z+2.0*mead_d
+
+# seleziono solo le nanoparticelle con il centro detro la box visto che poi 
+# dal calcolo della psd escludo comunque i punti ai bordi della box
+
+box_botx = px
+box_topx = px + 6.0*mead_d
+box_boty = py
+box_topy = py + 6.0*mead_d
+box_botz = pz
+box_topz = pz + 6.0*mead_d
+
+nanopscx, nanopscy, nanopscz = \
+        nanoparticle.nanoparticle_to_arrays (nanoparticles)
+
+bools1 = (nanopscx >= box_botx)
+bools2 = (nanopscy >= box_boty)
+bools3 = (nanopscz >= box_botz)
+
+bools4 = (nanopscx <= box_topx)
+bools5 = (nanopscy <= box_topy)
+bools6 = (nanopscz <= box_topz)
+
+interior_indices, = numpy.where(bools1*bools2*bools3*bools4*bools5*bools6)
+
+selected_nanoparticles = []
+
+filetoprint = open("selected_nanop.txt", "w")
+
+for id in interior_indices:
+  selected_nanoparticles.append(nanoparticles[id])
+
+  cx, cy, cz, A, B, H, p2x, p2y, p2z, tetha = \
+      nanoparticles[id].get_to_print()
+
+  data = str(cx) + " " + \
+         str(cy) + " " + \
+         str(cz) + " " + \
+         str(A) + " " + \
+         str(B) + " " + \
+         str(H) + " " + \
+         str(p2x) + " " + \
+         str(p2y) + " " + \
+         str(p2z) + " " + \
+         str(tetha) + "\n"
+
+  filetoprint.write(data)
+
+filetoprint.close()
+
 vfile = open("void.txt", "w")
 
-numof = 500
+numof = 100
 
-dx = (maxbox_x - minbox_x)/(numof+1) 
-dy = (maxbox_y - minbox_y)/(numof+1)
-dz = (maxbox_z - minbox_z)/(numof+1)
+dx = (box_botx - box_topx)/(numof+1) 
+dy = (box_boty - box_topy)/(numof+1)
+dz = (box_botz - box_topz)/(numof+1)
 
 counter = 0
 px = botx - dx
@@ -97,7 +159,7 @@ for i in range(numof):
       pz += dz
       counter = counter + 1
 
-      if (is_in_the_void_nanoparticle(nanoparticles, \
+      if (is_in_the_void_nanoparticle(selected_nanoparticles, \
             px, py, pz)):
         line = str(px) + " " + str(py) + " " + str(pz) + " 0.50\n"
         vfile.write(line)
