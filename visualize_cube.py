c920caa65ffb32fc7d6a7fdfbfb4f17afdb9c3e7
Loriano Storchi <loriano@storchi.org>
2015-07-13 09:41:09 +0200

    add the rotation to visualizer

--- visualize_cube.py
+++ visualize_cube.py
@@ -33,36 +33,23 @@ cubes = []
 file = open(filename, "r")
 
 for sp in file:
-  cx, cy, cz, dim = sp.split(" ")
+  cx, cy, cz, dim, px, py, pz, tetha = sp.split(" ")
   cub = cube_fill.cube(float(cx), float(cy), \
       float(cz), float(dim))
-
-  # test rotate  
-  """
-  cub2 = cube_fill.cube(float(cx), float(cy), \
-      float(cz), float(dim))
-  tetha = random.uniform(0.0, 2.0*math.pi)
-  x = random.uniform(0.0, 300.0)
-  y = random.uniform(0.0, 300.0)
-  z = random.uniform(0.0, 300.0)
-  #p = point.point(float(cx), float(cy), float(cz) + 10.0)
-  p = point.point(x, y, z)
-  cub2.rotate(p, tetha)
-  cubes.append(cub2)
-  """
-
+  p = point.point(float(px), float(py), float(pz))
+  cub.rotate(p, float(tetha))
   cubes.append(cub)
 
 file.close()
 
 actors = []
 
-p = [97.8826536512+0.4, 111.442677785+0.46, 111.202123474+0.49]
-ptc = point.point(p[0], p[1], p[2])
-actors.append(ptc.get_actor())
+#p = [97.8826536512+0.4, 111.442677785+0.46, 111.202123474+0.49]
+#ptc = point.point(p[0], p[1], p[2])
+#actors.append(ptc.get_actor())
 
 for cub in cubes:
-  print "is inside: ", cub.is_point_inside(p)
-  actors.append(cub.get_vtk_actor(0.5, 0.6, 0.1, 0.8))
+  #print "is inside: ", cub.is_point_inside(p)
+  actors.append(cub.get_vtk_actor(0.5, 0.6, 0.1, 1.0))
 
 visualize_nanop.visualize_actors (actors)
