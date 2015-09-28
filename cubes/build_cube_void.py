97d2ed92420a2ddd58cf5ba189fa3beec6c0326c
Loriano Storchi <loriano@storchi.org>
2015-09-28 16:48:23 +0200

    improved several approches

--- cubes/build_cube_void.py
+++ cubes/build_cube_void.py
@@ -22,6 +22,10 @@ import vtk
 
 import cubes_utility
 
+CUBE_DIM = 0.633
+CUBERADIUSLIMIT = CUBE_DIM*math.sqrt(3.0)
+CUBERADIUSLIMIT = CUBE_DIM
+
 ###############################################################################
 
 def inside_any_cubes (cub, cubes, cubcenterx, cubcentery, cubcenterz, 
@@ -30,23 +34,29 @@ def inside_any_cubes (cub, cubes, cubcenterx, cubcentery, cubcenterz,
   cx, cy, cz = cub.get_center()
 
   bools1 = numpy.sqrt((cubcenterx - cx)**2 + (cubcentery - cy)**2 +  \
-      (cubcenterz - cz)**2) <= 2.0*cubradius
+      (cubcenterz - cz)**2) <= 2.0*CUBERADIUSLIMIT
   interior_indices, = numpy.where(bools1)
 
-  for idx in interior_indices:
-    if (cubes[idx].is_point_inside([cx, cy, cz])):
-      return True
+  #print numpy.sqrt((cubcenterx - cx)**2 + (cubcentery - cy)**2 +\
+  #    (cubcenterz - cz)**2)
 
-  for idx in interior_indices:
-    for p in cub.get_cube_coordintes():
-      if (cubes[idx].is_point_inside(p)):
+  #for i in range(len(cubcenterx)):
+  #  d = math.sqrt((cubcenterx[i] - cx)**2 + (cubcentery[i] - cy)**2 + \
+  #      (cubcenterz[i] - cz)**2)
+  #  if (d <= 2.0*CUBE_DIM*math.sqrt(3.0)):
+  #     if (cubes[i].get_tagnumber() != \
+  #         cub.get_tagnumber()):
+  #       return True
+
+  #print interior_indices, len(interior_indices)
+
+  if (len(interior_indices) > 0):
+    for idx in interior_indices:
+      #print cubes[idx].get_tagnumber(), " vs ", cub.get_tagnumber()
+      if (cubes[idx].get_tagnumber() != \
+          cub.get_tagnumber()):
         return True
-
-  for idx in interior_indices:
-    for p in cubes[idx].get_cube_coordintes():
-      if (cub.is_point_inside(p)):
-        return True
-
+  
   return False
 
 ###############################################################################
@@ -97,17 +107,27 @@ def silentremove(filename):
 
 ###############################################################################
 
-# non mi interessano le intersezioni
-nanoparticle.POINTINSIDEDIM = 0
+def is_very_close (newcenter, cubcenterx, cubcentery, cubcenterz):
 
-NUM_OF_STARTING_CUBE = 20
+  cx = newcenter[0]
+  cy = newcenter[1]
+  cz = newcenter[2]
+
+  bools1 = numpy.sqrt((cubcenterx - cx)**2 + (cubcentery - cy)**2 +  \
+      (cubcenterz - cz)**2) <= CUBE_DIM/2.0
+
+  interior_indices, = numpy.where(bools1)
+
+  return (len(interior_indices) != 0)
+
+###############################################################################
+
+NUM_OF_STARTING_CUBE = 4
 MAX_NUM_OF_CUBE = 10*NUM_OF_STARTING_CUBE
-
-CUBE_DIM = 0.633
 
 cubesets = []
 
-zmax = xmax = ymax = 20.0
+zmax = xmax = ymax = 10*CUBE_DIM
 zmin = xmin = ymin = 0.0
 
 # questo potrebbe essere un buon modo per trovare un massimo di numeri
@@ -117,6 +137,8 @@ NZN = int ((zmax-zmin)/CUBE_DIM) + 1
 
 MAX_NUM_OF_CUBE = NXN * NYN * NZN
 
+#MAX_NUM_OF_CUBE = 10*NUM_OF_STARTING_CUBE
+
 print "MAX_NUM_OF_CUBE: ", MAX_NUM_OF_CUBE
 
 print >> sys.stderr, "Box limits: ", xmin, xmax, ymin, ymax, zmin, zmax
@@ -124,10 +146,12 @@ print >> sys.stderr, "Box limits: ", xmin, xmax, ymin, ymax, zmin, zmax
 # voglio fermarmi a circa 2 D dalla vetta visto che in cima avro' sempre una
 # densita' minore(ricorda la prima sfera che supera zmax ferma la procedura.
 
-#actors = []
+visactors = True
+actors = []
 
-#actors.extend(cube.cube_to_actors (xmin, ymin, zmin, \
-#    xmax, ymax, zmax))
+if (visactors):
+  actors.extend(cube.cube_to_actors (xmin, ymin, zmin, \
+      xmax, ymax, zmax))
 
 cubcenterx = numpy.empty(0)
 cubcentery = numpy.empty(0)
@@ -138,6 +162,7 @@ centers = []
 cubes = []
 
 globalindex = 0
+
 
 print "First step... ", globalindex, " ", len(cubesets)
 
@@ -163,10 +188,10 @@ while (j < (NUM_OF_STARTING_CUBE/2)):
 
     # check if any point of the cubes is inside not perfect but good enough
 
+    cub.set_tagnumber (globalindex)
+
     if (not inside_any_cubes (cub, cubes, cubcenterx, 
       cubcentery, cubcenterz, cubradius)):
-
-      cub.set_tagnumber (globalindex)
 
       cubes.append(cub)
       cubesets.append([globalindex])
@@ -180,7 +205,8 @@ while (j < (NUM_OF_STARTING_CUBE/2)):
       j = j + 1
       globalindex = globalindex + 1
 
-      #actors.append(cub.get_vtk_actor(0.5, 0.6, 0.1))
+      if (visactors):
+        actors.append(cub.get_vtk_actor(0.5, 0.6, 0.1))
 
 print "Second step... ", globalindex, " ", len(cubesets)
 
@@ -199,10 +225,10 @@ while (j < (NUM_OF_STARTING_CUBE/2)):
 
     # check if any point of the cubes is inside not perfect but good enough
 
+    cub.set_tagnumber (globalindex)
+
     if (not inside_any_cubes (cub, cubes, cubcenterx, 
       cubcentery, cubcenterz, cubradius)):
-
-      cub.set_tagnumber (globalindex)
 
       cubes.append(cub)
       cubesets.append([globalindex])
@@ -216,18 +242,21 @@ while (j < (NUM_OF_STARTING_CUBE/2)):
       j = j + 1
       globalindex = globalindex + 1
 
-      #actors.append(cub.get_vtk_actor(0.5, 0.6, 0.1))
+      if (visactors):
+        actors.append(cub.get_vtk_actor(0.5, 0.6, 0.1))
 
 print "Third step... ", globalindex, " ", len(cubesets)
 
 numofloop = 0
 i = len(cubes)
-while ((i < MAX_NUM_OF_CUBE) and (numofloop < 8 * MAX_NUM_OF_CUBE)) :
+while ((i < MAX_NUM_OF_CUBE) and (numofloop < 4 * MAX_NUM_OF_CUBE)) :
 
   print "  ", i , " of " , MAX_NUM_OF_CUBE
-  print "    ", numofloop, " of " , 8 * MAX_NUM_OF_CUBE
+  print "    ", numofloop, " of " , 4 * MAX_NUM_OF_CUBE
 
   oldnumof = len(cubes)
+
+  numofloop = numofloop + 1
 
   for cubi in range(oldnumof):
     #print "      ", cubi , " of ", oldnumof
@@ -240,11 +269,10 @@ while ((i < MAX_NUM_OF_CUBE) and (numofloop < 8 * MAX_NUM_OF_CUBE)) :
       while (added == False): 
 
         maxnumoftry = maxnumoftry + 1
-        numofloop = numofloop + 1
 
         #print "         ", maxnumoftry, " of 7"
 
-        # se dopo 7 tentativi non risco a poszionare mi blocco
+        # se dopo 7 tentativi non risco a posizionare mi blocco
         if (maxnumoftry == 7):
           added = True
 
@@ -266,29 +294,37 @@ while ((i < MAX_NUM_OF_CUBE) and (numofloop < 8 * MAX_NUM_OF_CUBE)) :
           
             # dovrebbe essere un modo semplice per vedere se il cubo si sovrappone
             if (not (newcenter in centers)):
-            
-              newcub = cube_fill.cube(newcenter[0], newcenter[1], newcenter[2], dim)
-              newcub.set_iface (occupiediface)
-              newcub.set_points (p1, p2, p3, p4, p5, p6, p7, p8)
-           
-              if (not inside_any_cubes (newcub, cubes, cubcenterx, \
-                cubcentery, cubcenterz, cubradius)):
+              # faccio il check tra quelli dello stesso seme cosi' in pratica
+              # cosi' posso poi fare un check con sfere per gli altri semi.
+              if (not (is_very_close (newcenter, cubcenterx, cubcentery, cubcenterz))):
 
+                newcub = cube_fill.cube(newcenter[0], newcenter[1], newcenter[2], dim)
+                newcub.set_iface (occupiediface)
+                newcub.set_points (p1, p2, p3, p4, p5, p6, p7, p8)
+                # devo settare il tag prima
                 newcub.set_tagnumber(cubes[cubi].get_tagnumber())
-                cubesets[cubes[cubi].get_tagnumber()].append(i)
+                
+                if (not inside_any_cubes (newcub, cubes, cubcenterx, \
+                  cubcentery, cubcenterz, cubradius)):
+                
+                  cubesets[cubes[cubi].get_tagnumber()].append(i)
+                  
+                  i = i + 1
+                  
+                  cubes.append(newcub)
+                  centers.append(newcenter)
+                  cubcenterx = numpy.append(cubcenterx, newcenter[0])
+                  cubcentery = numpy.append(cubcentery, newcenter[1])
+                  cubcenterz = numpy.append(cubcenterz, newcenter[2])
+                  cubradius = numpy.append(cubradius, cub.get_radius()) 
+                  
+                  added = True
+                  
+                  if (visactors):
+                    actors.append(newcub.get_vtk_actor(0.5, 0.6, 0.1))
 
-                i = i + 1
+print "Total num of cubes: " , len(cubes)
 
-                cubes.append(newcub)
-                centers.append(newcenter)
-                cubcenterx = numpy.append(cubcenterx, x)
-                cubcentery = numpy.append(cubcentery, y)
-                cubcenterz = numpy.append(cubcenterz, z)
-                cubradius = numpy.append(cubradius, cub.get_radius()) 
-          
-                added = True
-           
-                #actors.append(newcub.get_vtk_actor(0.5, 0.6, 0.1))
 
 
 silentremove("full.xyz")
@@ -302,4 +338,5 @@ for i in range(len(cubesets)):
   silentremove(str(i)+".xyz")
   cubes_utility.cubes_to_xyzfile(cubs_to_print, str(i)+".xyz")
 
-#visualize_nanop.visualize_actors (actors)
+if (visactors):
+  visualize_nanop.visualize_actors (actors)
