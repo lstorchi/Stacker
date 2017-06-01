102c1dfcb0b0f267e088d5cb06ead5f9137b7553
Loriano Storchi <loriano@storchi.org>
2017-06-01 12:04:49 +0200

    backup

--- separate/convert.py
+++ separate/convert.py
@@ -20,19 +20,55 @@ import line
 
 DELTA = 0.529177249
 
-filename = ""
+xyzfname = ""
+dftboutf = ""
 
-if (len(sys.argv)) == 2:
-  filename = sys.argv[1]
+if (len(sys.argv)) == 3:
+  xyzfname = sys.argv[1]
+  dftboutf = sys.argv[2]
 else:
-  print "usage :", sys.argv[0] , " xyzfile"
+  print "usage :", sys.argv[0] , " xyzfile dftb+detailed.out "
   exit(1)
 
-xlist, ylist, zlist, atoms = xyznanop.read_ncxyz (filename, False)
+xlist, ylist, zlist, atoms = xyznanop.read_ncxyz (xyzfname, False)
 
-for i in range(len(xlist)):
-    xlist[i] = DELTA * xlist[i]
-    ylist[i] = DELTA * ylist[i]
-    zlist[i] = DELTA * zlist[i]
- 
+fp = open(dftboutf, "r")
+
+coords = []
+start = False
+for l in fp:
+
+    if (l.find("Net atomic charges (e)") != -1 ):
+        start = False
+
+    if start:
+        coords.append(l)
+
+    if (l.find("Coordinates of moved atoms (au):") != -1 ):
+        start = True
+
+fp.close()
+
+atomslist = []
+
+for l in coords:
+    p = re.compile(r'\s+')
+    line = p.sub(' ', l)
+    line = line.lstrip()
+    line = line.rstrip()
+                     
+    plist = line.split(" ")
+
+    if len(plist) == 4:
+        atomslist.append((int(plist[0])-1, \
+                float(plist[1])*DELTA, float(plist[2])*DELTA, \
+                float(plist[3])*DELTA))
+
+for a in atomslist:
+    idx = a[0]
+
+    xlist[idx] = a[1]
+    ylist[idx] = a[2]
+    zlist[idx] = a[3]
+
 xyznanop.write_ncxyz ("out.xyz", xlist, ylist, zlist, atoms)
