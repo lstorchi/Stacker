8f88cb1e1d2337f4356942dcfc3f93ed5aaa3cfb
Loriano Storchi <loriano@storchi.org>
2017-09-19 10:21:34 +0200

    backup

--- analyse3d/copyoriginal.sh
+++ analyse3d/copyoriginal.sh
@@ -1,16 +1,13 @@
-
-> out
-
 for i in {0..39}
 do 
+  > out."$i" 
+
   for name in $(ls ../../step4/"$i"_near/)
   do 
-    python checkrmsd.py ../../step4/"$i"_near/"$name"  ./cluster_"$i".xyz  >> out 
+    python checkrmsd.py ../../step4/"$i"_near/"$name"  ./cluster_"$i".xyz  >> out."$i"
   done
 
-  export TOCPY=$(sort -k 3 -n out  | head -n 1 | awk '{print $1}')
+  export TOCPY=$(sort -k 3 -g out."$i"  | head -n 1 | awk '{print $1}')
   cp $TOCPY ./
 done
-
-rm -f out
 
---
+++ analyse3d/extract_pair_solid.py
@@ -0,0 +1,52 @@
+import re
+import sys 
+import math
+import scipy 
+import numpy
+
+from scipy.spatial import distance
+from scipy import cluster
+
+sys.path.append("../modules")
+
+import nanoparticle
+import xyznanop
+import common
+import sphere
+import point
+import util
+import line
+
+filename = ""
+nanopfile = ""
+nanoparticle.POINTINSIDEDIM = 0
+
+if (len(sys.argv)) == 3:
+  filename = sys.argv[1]
+  nanopfile = sys.argv[2]
+else:
+  print "usage :", sys.argv[0] , " listacoppie nanopfile.txt"
+  exit(1)
+
+fp = open(nanopfile)
+allstring = fp.readlines()
+fp.close()
+
+
+fp = open(filename)
+for l in fp:
+    subtokens = l.split("_")
+    if len(subtokens) != 4:
+        print "Error ", subtokens
+        exit(1)
+
+    clusterid = int(subtokens[1])
+    id1 = int(subtokens[2])
+    id2 = int(re.sub('\.xyz$', '', subtokens[-1]))
+     
+    fpwrt = open(str(clusterid) + ".txt", "w")
+    fpwrt.write(allstring[id1])
+    fpwrt.write(allstring[id2])
+    fpwrt.close() 
+
+fp.close()
