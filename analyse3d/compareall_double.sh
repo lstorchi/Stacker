7aeaa834bc41e3fbb5e078c8c1e0a1a84afafa85
Loriano Storchi <loriano@storchi.org>
2017-10-23 15:08:29 +0200

    backup

--- analyse3d/compareall_double.sh
+++ analyse3d/compareall_double.sh
@@ -2,9 +2,10 @@ for i in {0..39}
 do 
   export name1=$(ls ./step8/cluster_"$i"_*_opt.xyz)  
   export name2=$(ls ./step8/cluster_"$i"_*.xyz)
-  export name3=$(ls ./step8/cluster_"$i"_*_*.xyz)
   
-  python checkrmsd.py $name1 $name2 
-  python checkrmsd.py $name1 $name3
+  for n in $name2
+  do
+    python checkrmsd.py $name1 $n
+  done
 
 done
