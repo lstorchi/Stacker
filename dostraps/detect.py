f989cbede657bab50cfaacbf26ffc017d7986000
Loriano Storchi <loriano@storchi.org>
2018-12-05 16:59:46 +0100

    backup

--- dostraps/detect.py
+++ dostraps/detect.py
@@ -9,7 +9,7 @@ import scipy.interpolate
 
 import matplotlib.pyplot as plt
 
-from scipy.interpolate import UnivariateSpline
+from scipy import interpolate
 
 if (len(sys.argv)) == 2: 
     filename = sys.argv[1]
@@ -34,18 +34,16 @@ for l in fp:
 
 fp.close()
 
-y_spl = UnivariateSpline(x,y,s=0,k=4)
+#f = interpolate.interp1d(x, y)
+f = numpy.polyfit(x, y, 1)
+m, b = f
 
-plt.plot(x,y,'ro',label = 'data')
+
+plt.plot(x, y,'ro',label = 'data')
 x_range = numpy.linspace(x[0],x[-1],1000)
-plt.plot(x_range,y_spl(x_range))
+for i in range(len(x_range)):
+        plt.plot(x_range[i], x_range[i] * m + b, 'go')
 
-y_spl_2d = y_spl.derivative(n=2)
-y_dev = y_spl_2d(x_range)
-
-plt.plot(x_range,y_dev)
-
-# dal plot delle derivate secone e dal grafico vedo il flesso 
-# piu' ragionevole
+print -b/m
 
 plt.show()
