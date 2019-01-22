4e6c42ddc9e1a7e1a6ba41f1f0af6aed0034c12c
Loriano Storchi <loriano@storchi.org>
2019-01-22 15:15:41 +0100

    adding traps computer

--- dostraps/placetraps.py
+++ dostraps/placetraps.py
@@ -1,5 +1,17 @@
 import re
 import sys
+import vtk
+import numpy
+
+from scipy.interpolate import interp1d
+
+sys.path.append("../modules")
+
+import xyznanop
+import sphere
+import point
+import util
+import cube
 
 #######################################################################`
 
@@ -53,4 +65,143 @@ else:
 x, xval, sumx = read_filecurve (xcurvefname)
 y, yval, sumy = read_filecurve (ycurvefname)
 z, zval, sumz = read_filecurve (zcurvefname)
+
+centerx = numpy.mean(x)
+centery = numpy.mean(y)
+centerz = numpy.mean(z)
+
+x -= centerx
+y -= centery
+z -= centerz
+
+print "Curves limits: "
+print min(x), max(x) 
+print min(y), max(y) 
+print min(z), max(z)
+
+fx = interp1d(x, xval, kind='cubic')
+fy = interp1d(y, yval, kind='cubic')
+fz = interp1d(z, zval, kind='cubic')
+
+"""
+xnew = numpy.linspace(min(x), max(x), num=10000, endpoint=True)
+ynew = fx(xnew)
+import matplotlib.pyplot as plt
+plt.plot(x, xval, 'o', xnew, ynew, '-')
+plt.show()
+"""
+
+xlist, ylist, zlist, atoms = xyznanop.read_ncxyz (xyzfname)
+
+np_centerx = numpy.mean(xlist)
+np_centery = numpy.mean(ylist)
+np_centerz = numpy.mean(zlist)
+
+xlist -= np_centerx
+ylist -= np_centery
+zlist -= np_centerz
+
+print "Nanoparticle limits: "
+print min(xlist), max(xlist)
+print min(ylist), max(ylist)
+print min(zlist), max(zlist)
+
+traps_xyz_pdf = []
+totp = 0.0
+for i in range(len(atoms)):
+    #print atoms[i]
+    if atoms[i] == "Ti":
+        xv = xlist[i]
+        yv = ylist[i]
+        zv = zlist[i]
+
+        if xv < min(x):
+            print "out of range"
+            xv = min(x)
+        if xv > max(x):
+            print "out of range"
+            xv = max(x)
+
+        if yv < min(y):
+            print "out of range"
+            yv = min(y)
+        if yv > max(y):
+            print "out of range"
+            yv = max(y)
+
+        if zv < min(z):
+            print "out of range"
+            zv = min(z)
+        if zv > max(z):
+            print "out of range"
+            zv = max(z)
+
+        p = fx(xv) * fy(yv) * fz(zv)
+        totp += p
+        traps_xyz_pdf.append([xlist[i], ylist[i], zlist[i], p])
+
+for i in range(len(traps_xyz_pdf)):
+    traps_xyz_pdf[i][3] = traps_xyz_pdf[i][3] / totp 
+
+# create a rendering window and renderer
+ren = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+ 
+# create a renderwindowinteractor
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+sources = []
+
+for trap in traps_xyz_pdf:
+  source = vtk.vtkSphereSource()
+  source.SetCenter(trap[0], trap[1], trap[2])
+  source.SetRadius(trap[3])
+
+  sources.append(source)
+
+# mapper
+mappers = []
+
+for source in sources:
+  mapper = vtk.vtkPolyDataMapper()
+  #mapper.SetInput(source.GetOutput())
+  mapper.SetInputConnection(source.GetOutputPort())
+  
+  mappers.append(mapper)
+
+# actor
+actors = []
+
+for mapper in mappers:
+  actor = vtk.vtkActor()
+  #actor.GetProperty().SetOpacity(0.5)
+  actor.SetMapper(mapper)
+
+  actors.append(actor);
+ 
+# assign actor to the renderer
+
+for actor in actors:
+  ren.AddActor(actor)
+
+# enable user interface interactor
+try:
+  iren.Initialize()
+  renWin.Render()
+  #writer = vtk.vtkGL2PSExporter()
+  #writer.SetRenderWindow(renWin)
+  #writer.SetFileFormatToSVG ()
+  #writer.SetFilePrefix("largeImage")
+  #writer.Write()
+  iren.Start()
+except Exception as e:
+  print e
+
+#for trap in traps_xyz_pdf:
+#    print trap[0], trap[1], trap[2], trap[3]
+
+numoftraps = 10000
+random_array = numpy.random.uniform(0.0, 1.0, numoftraps)
 
