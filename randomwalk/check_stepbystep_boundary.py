120be00616fc54ce61843d8f1c3f4a99ca33f0f5
Loriano Storchi <loriano@storchi.org>
2020-01-27 16:53:15 +0100

    read and resort

--- randomwalk/check_stepbystep_boundary.py
+++ randomwalk/check_stepbystep_boundary.py
@@ -1,6 +1,11 @@
 from os import listdir
 from os.path import isfile, join
 
+import argparse
+import sys
+
+import convert_boundary_cond
+
 #####################################################################
 
 def is_an_int(s):
@@ -14,6 +19,25 @@ def is_an_int(s):
 #####################################################################
 
 mypath = "/pub3/redo/"
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-p","--filepath", help="Electron filename root path", \
+                    type=str, required=True)
+parser.add_argument("-d","--dimensions", help="dimension \"xdim:ydim:zdim\"", \
+                    type=str, required=True)
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    exit(1)
+
+args = parser.parse_args()
+mypath = args.filepath
+sxdim, sydim, szdim = args.dimensions.split(":")
+
+xdim = float(sxdim)
+ydim = float(sydim)
+zdim = float(szdim)
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
@@ -35,4 +59,29 @@ for f in onlyfiles:
                 num_of_ele = int(val)
 
 for s in sorted(steps):
-    print("need to check at steps ", s, " for ", num_of_ele, " electrons ")
+    print("need to check at steps %6d"%(s), " for %5d"%(num_of_ele), " electrons ")
+
+    for i in range(1, num_of_ele+1):
+        filename = mypath+"electrons_%d_of_%d_at_step_%d.txt"%(i, num_of_ele, s)
+        print(filename)
+
+        file = open(filename, "r")
+
+        coordinates = []
+        for line in file:
+            mergedline = ' '.join(line.split())
+            sx, sy, sz, snpnum, stpidx, strapid = mergedline.split(" ")
+
+            x = float(sx)
+            y = float(sy)
+            z = float(sz)
+
+            coordinates.append([x, y, z, int(snpnum), int(stpidx), int(strapid)])
+
+        file.close()
+        final = convert_boundary_cond.resort_boundary (xdim, ydim, zdim, coordinates)
+
+        for f in final:
+            print("%10.5f %10.5f %10.5f %10d %10d"%(f[0], f[1], f[2], f[3], f[4]))
+
+
